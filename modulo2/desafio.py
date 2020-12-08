792b1351ffc56275d6a88898e77322e0b6914a13
Gabriel Leitão <xgabriel.rf@gmail.com>
2020-12-08 19:30:58 -0300

    Letra f

--- modulo2/desafio.py
+++ modulo2/desafio.py
@@ -17,10 +17,13 @@ df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
 #print(df.loc[df['datetime'] <= '31/12/2011', ['datetime']])
 
 # e. Quantos registros existem para o ano de 2012?
-df['datetime'] = pd.to_datetime(df['datetime'])
-print(df.loc[df['datetime'] > '31/12/2011', ['datetime']])
+#df['datetime'] = pd.to_datetime(df['datetime'])
+#print(df.loc[df['datetime'] > '31/12/2011', ['datetime']])
 
 # f. Quantas locações de bicicletas foram efetuadas em 2011?
+df['datetime'] = pd.to_datetime(df['datetime'])
+print(np.sum(df.loc[df['datetime'] <= '31/12/2011', ['total_count']]))
+
 # g. Quantas locações de bicicletas foram efetuadas em 2012?
 # h. Qual estação do ano contém a maior média de locações de bicicletas?
 # i. Qual estação do ano contém a menor média de locações de bicicletas?
