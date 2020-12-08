388e04255e45d154fea21689ac87074046e19447
Gabriel Leitão <xgabriel.rf@gmail.com>
2020-12-08 19:04:37 -0300

    Letra a

---
+++ modulo2/desafio.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
+
+# a. Qual o tamanho desse dataset?
+print(df.shape)
+
+# b. Qual a média da coluna windspeed?
+# c. Qual a média da coluna temp?
+# d. Quantos registros existem para o ano de 2011?
+# e. Quantos registros existem para o ano de 2012?
+# f. Quantas locações de bicicletas foram efetuadas em 2011?
+# g. Quantas locações de bicicletas foram efetuadas em 2012?
+# h. Qual estação do ano contém a maior média de locações de bicicletas?
+# i. Qual estação do ano contém a menor média de locações de bicicletas?
+# j. Qual horário do dia contém a maior média de locações de bicicletas?
+# k. Qual horário do dia contém a menor média de locações de bicicletas?
+# l. Que dia da semana contém a maior média de locações de bicicletas?
+# m. Que dia da semana contém a menor média de locações de bicicletas?
+# n. Às quartas-feiras (weekday = 3), qual o horário do dia contém a maior média de locações de bicicletas?
+# o. Aos sábados (weekday = 6), qual o horário do dia contém a maior média de locações de bicicletas?
