773028f89e781f1d178589eb846768f4fba0496b
Gabriel Leitão <xgabriel.rf@gmail.com>
2020-12-08 20:37:32 -0300

    Letra m

--- modulo2/desafio.py
+++ modulo2/desafio.py
@@ -79,13 +79,33 @@ df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
 ##RESPOSTA: E o menor valor foi 4428 na hora 4
 
 # l. Que dia da semana contém a maior média de locações de bicicletas?
-maior_valor = 0
+#maior_valor = 0
+#for weekday in range(0,7):
+#    total = np.sum(df.loc[df["weekday"] == weekday, ["total_count"]])
+#    total_split = str(total).split()
+#    print(f'Na semana {weekday}, {total_split[1]}')
+#    if int(maior_valor) < int(total_split[1]):
+#        maior_valor = total_split[1]
+#        semana = weekday
+#dia_semana = {
+#    0: 'domingo',
+#    1: 'segunda-feira',
+#    2: 'terça-feira',
+#    3: 'quarta-feira',
+#    4: 'quinta-feira',
+#    5: 'sexta-feira',
+#    6: 'sábado'}
+#print(f'\nE o maior valor foi {maior_valor} em {dia_semana[semana]}')
+##RESPOSTA: E o maior valor foi 487790 em sexta-feira
+
+# m. Que dia da semana contém a menor média de locações de bicicletas?
+menor_valor = 999999
 for weekday in range(0,7):
     total = np.sum(df.loc[df["weekday"] == weekday, ["total_count"]])
     total_split = str(total).split()
     print(f'Na semana {weekday}, {total_split[1]}')
-    if int(maior_valor) < int(total_split[1]):
-        maior_valor = total_split[1]
+    if int(menor_valor) > int(total_split[1]):
+        menor_valor = total_split[1]
         semana = weekday
 dia_semana = {
     0: 'domingo',
@@ -95,9 +115,8 @@ dia_semana = {
     4: 'quinta-feira',
     5: 'sexta-feira',
     6: 'sábado'}
-print(f'\nE o maior valor foi {maior_valor} em {dia_semana[semana]}')
-#RESPOSTA: E o maior valor foi 487790 em sexta-feira
+print(f'\nE o menor valor foi {menor_valor} em {dia_semana[semana]}')
+#RESPOSTA: E o menor valor foi 444027 em domingo
 
-# m. Que dia da semana contém a menor média de locações de bicicletas?
 # n. Às quartas-feiras (weekday = 3), qual o horário do dia contém a maior média de locações de bicicletas?
 # o. Aos sábados (weekday = 6), qual o horário do dia contém a maior média de locações de bicicletas?
