03296ec02235191115a35bd773823b210e60f229
Gabriel Leitão <xgabriel.rf@gmail.com>
2020-12-08 19:14:15 -0300

    Letra d

--- modulo2/desafio.py
+++ modulo2/desafio.py
@@ -10,9 +10,12 @@ df = pd.read_csv("https://pycourse.s3.amazonaws.com/bike-sharing.csv")
 #print(np.mean(df['windspeed']))
 
 # c. Qual a média da coluna temp?
-print(np.mean(df['temp']))
+#print(np.mean(df['temp']))
 
-# d. Quantos registros existem para o ano de 2011?
+# d. Quantos registros existem para o ano de 2011? 
+df['datetime'] = pd.to_datetime(df['datetime'])
+print(df.loc[df['datetime'] <= '31/12/2011',['datetime']])
+
 # e. Quantos registros existem para o ano de 2012?
 # f. Quantas locações de bicicletas foram efetuadas em 2011?
 # g. Quantas locações de bicicletas foram efetuadas em 2012?
