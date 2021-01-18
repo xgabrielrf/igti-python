68f55e01acad1fc9417099b6713f713bcd2b5f17
Gabriel Leitão <xgabriel.rf@gmail.com>
2021-01-18 03:54:20 -0300

    Criando formulário e migrando as infos para o db

--- modulo3/igtiflixweb/genero/views.py
+++ modulo3/igtiflixweb/genero/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+def cadastro(request):
+    form = forms.GeneroForm()
+    data_dict = {'form':form}
+    return render(request,'genero/genero.html', data_dict)
